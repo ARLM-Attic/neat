3bd511803781e3d55b65d498128ed2eedf5683e5
SND\saeedoo_cp <SND\saeedoo_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2010-11-16 14:01:45 +0000

    git-svn-id: https://neat.svn.codeplex.com/svn@59498 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ Neat/NeatStarter/NeatStarter/StartScreen.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+#if WINDOWS_PHONE
+using Microsoft.Xna.Framework.Input.Touch;
+#endif
+#if WINDOWS
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+#endif
+using Microsoft.Xna.Framework.Media;
+using Neat;
+using Neat.MenuSystem;
+using Neat.EasyMenus;
+using Neat.GUI;
+using Neat.Mathematics;
+using Neat.Graphics;
+
+namespace NeatStarter
+{
+    public class StartScreen : GamePart
+    {
+        float alpha ;
+        float fadeRate = 0.01f;
+        public string NextScreen = "";
+
+        public StartScreen(NeatGame Game)
+            : base(Game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Activate()
+        {
+            alpha = -0.8f;
+            base.Activate();
+        }
+
+        public override void Behave(GameTime gameTime)
+        {
+            alpha += fadeRate;
+            if (alpha > 2.0f && NextScreen.Trim().Length > 0)
+                game.console.Run("ap " + NextScreen);
+            base.Behave(gameTime);
+        }
+
+        public override void HandleInput(GameTime gameTime)
+        {
+            base.HandleInput(gameTime);
+        }
+
+
+        public override void Render(GameTime gameTime)
+        {
+            var t = game.getTexture("neatlogo");
+            Vector2 p = new Vector2( (game.gameWidth - t.Width) / 2f, (game.gameHeight - t.Height) / 2f);
+            game.GetEffect("ColorFilter").Parameters["mulColor"].SetValue(new Vector4(MathHelper.Clamp(alpha,0.0f,1.0f)));
+            game.UseEffect("ColorFilter");
+            game.spriteBatch.Draw(t, p, Color.White);
+            base.Render(gameTime);
+        }
+    }
+}
